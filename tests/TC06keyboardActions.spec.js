01d0252ccd0b4f748bf924b43ba91f57a53d91fe
Mahesh3232 <mahesh.aher1999@gmail.com>
2024-03-05 22:02:01 +0530

    KeyboardActions

---
+++ tests/TC06keyboardActions.spec.js
@@ -0,0 +1,9 @@
+const { test, expect } = require('@playwright/test')
+
+test('Verify keyboard actions in playwright',async({page})=>{
+    await page.goto('https://gotranscript.com/text-compare')
+    //await page.keyboard.down('PageDown')
+    await page.locator('textarea[name="text1"]').fill('I am learning Playwright with JavaScript')
+    await page.keyboard.press('Control+A')
+    await page.waitForTimeout(3000)
+})
