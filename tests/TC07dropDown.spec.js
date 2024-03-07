8a5e2a4d414dc8c32656b88bd1d4073d74f91328
Mahesh3232 <mahesh.aher1999@gmail.com>
2024-03-07 19:27:55 +0530

    DropDown

---
+++ tests/TC07dropDown.spec.js
@@ -0,0 +1,15 @@
+//Static dropdown
+//dynyamic dropdown
+const { test, expect } = require('@playwright/test')
+const exp = require('constants')
+
+test('Verify static dropdown in playwright',async({page})=>{
+    await page.goto('https://letcode.in/dropdowns')
+    await page.locator('#fruits').selectOption('2')
+    await expect(page.locator('p[class="subtitle"]')).toBeVisible()
+    await expect(page.locator('p[class="subtitle"]')).toHaveText('You have selected Orange')
+    await page.locator('#superheros').selectOption('bt')
+    await expect(page.locator('[class="subtitle"]').last()).toBeVisible()
+    await expect(page.locator('[class="subtitle"]').last()).toHaveText('You have selected Batman')
+    await page.waitForTimeout(4000)
+})
