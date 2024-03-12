882a9d4d1220ee9a5fae294148a3b02a6dec53b9
Mahesh3232 <mahesh.aher1999@gmail.com>
2024-03-12 19:30:59 +0530

    DropDown

--- tests/TC07dropDown.spec.js
+++ tests/TC07dropDown.spec.js
@@ -1,7 +1,7 @@
 //Static dropdown
 //dynyamic dropdown
 const { test, expect } = require('@playwright/test')
-const exp = require('constants')
+
 
 test('Verify static dropdown in playwright',async({page})=>{
     await page.goto('https://letcode.in/dropdowns')
@@ -11,5 +11,19 @@ test('Verify static dropdown in playwright',async({page})=>{
     await page.locator('#superheros').selectOption('bt')
     await expect(page.locator('[class="subtitle"]').last()).toBeVisible()
     await expect(page.locator('[class="subtitle"]').last()).toHaveText('You have selected Batman')
+    await page.locator('#country').selectOption('India')
+    await expect(page.locator('#country')).toHaveValue('India')
     await page.waitForTimeout(4000)
+})
+
+//Dynyamic Dropdown
+
+test.only('Verify redbus dynyamic dropdown in playwright',async({page})=>{
+    await page.goto('https://www.redbus.in/')
+    await page.locator('#src').fill('Pune',{delay:2000})
+    await page.waitForSelector('.placeHolderMainText')
+   // await page.waitForTimeout(5000)
+    let optionCount = await page.locator('.placeHolderMainText').count()
+    //let text = await (await page.locator('.placeHolderMainText')).last().textContent()
+    console.log(optionCount)
 })
