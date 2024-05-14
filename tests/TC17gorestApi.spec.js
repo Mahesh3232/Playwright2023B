2ef1c9cb2e245467868056f6190eb51ae35b0844
panache-chinmay <chinmaydeshpande010@gmail.com>
2024-05-14 19:57:30 +0530

    GorestAPI

--- tests/TC17gorestApi.spec.js
+++ tests/TC17gorestApi.spec.js
@@ -1,4 +1,6 @@
 const { test, expect, request } = require("@playwright/test");
+let id = undefined
+let emailId = `mahesh.aher${Math.floor(Math.random() * 868)}@gmail.com`
 
 test("GET api request", async ({ request }) => {
   let req = await request.get("https://gorest.co.in/public/v2/users", {
@@ -12,12 +14,11 @@ test("GET api request", async ({ request }) => {
   let res = await req.json();
   console.log(res);
   expect(res.length).toBe(10)
-  expect(res[0].id).toBe(6899908);
-  expect(res[0].name).toBe("Advaya Verma");
 });
 
 
-test.only('Post API request for create User', async ({ request }) => {
+test('Post API request for create User', async ({ request }) => {
+  console.log(emailId)
   let req2 = await request.post('https://gorest.co.in/public/v2/users', {
     headers: {
       "Accept": "application/json",
@@ -28,14 +29,51 @@ test.only('Post API request for create User', async ({ request }) => {
     data: {
       "name": "Mahesh Aher",
       "gender": "male",
-      "email": "mahesh.aher66@gmail.com",
+      "email": emailId,
       "status": "active"
     }
   })
   let res2 = await req2.json()
   console.log(res2)
+  id = res2.id
   await expect(req2.status()).toBe(201)
   await expect(res2.name).toBe('Mahesh Aher')
   await expect(res2.gender).toBe('male')
   await expect(res2.status).toBe('active')
+})
+
+test('PUT API request for create User', async ({ request }) => {
+  console.log(id)
+  let req3 = await request.put(`https://gorest.co.in/public/v2/users/${id}`, {
+    headers: {
+      "Accept": "application/json",
+      "Content-Type": "application/json",
+      "Authorization":
+        "Bearer d94c4cc808661fa96c75b56d4f63cb80084a4324ac1f85d8761e6d59e7af8fd6",
+    },
+    data: {
+      "name": "Akshay Aher",
+      "gender": "male",
+      "email": emailId,
+      "status": "inactive"
+    }
+  })
+  let res3 = await req3.json()
+  console.log(res3)
+  await expect(req3.status()).toBe(200)
+  await expect(res3.name).toBe('Akshay Aher')
+  await expect(res3.gender).toBe('male')
+  await expect(res3.status).toBe('inactive')
+})
+
+test('DELETE API request for create User', async ({ request }) => {
+  let req4 = await request.delete(`https://gorest.co.in/public/v2/users/${id}`, {
+    headers: {
+      "Accept": "application/json",
+      "Content-Type": "application/json",
+      "Authorization":
+        "Bearer d94c4cc808661fa96c75b56d4f63cb80084a4324ac1f85d8761e6d59e7af8fd6",
+    }
+  })
+  await expect(req4.status()).toBe(204)
 })
